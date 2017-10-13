549642e737497b005b48f64aa4ab029137000715
Steven <steven@ceriously.com>
2017-10-13 16:13:48 -0400

    Convert copee to use ESM

---
+++ src/copee.js
@@ -0,0 +1,57 @@
+/*! *****************************************************************************
+https://github.com/styfle/copee
+Copyright (c) styfle. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+***************************************************************************** */
+/**
+ * Copy text to the user's clipboard
+ */
+export function toClipboard(text) {
+    // Create element and select the text
+    const ta = document.createElement('textarea');
+    ta.value = text;
+    ta.cols = 1;
+    ta.rows = 1;
+    ta.style.color = 'transparent';
+    ta.style.border = 'none';
+    document.body.appendChild(ta);
+    ta.select();
+    let success = false;
+    try {
+        success = document.execCommand('copy');
+    }
+    catch (err) {
+        success = false;
+    }
+    // Cleanup the element we created
+    document.body.removeChild(ta);
+    return success;
+}
+/**
+ * Copy text from an input/textarea element to a user's clipboard
+ * @param el
+ * @param preserveSelection
+ */
+export function fromElement(el, preserveSelection) {
+    let start = 0;
+    let end = 0;
+    if (preserveSelection) {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    }
+    el.select();
+    var success = false;
+    try {
+        success = document.execCommand('copy');
+    }
+    catch (err) {
+        success = false;
+    }
+    if (preserveSelection) {
+        el.setSelectionRange(start, end);
+    }
+    return success;
+}
+//# sourceMappingURL=copee.js.map
---
+++ src/copee.ts
@@ -0,0 +1,65 @@
+﻿/*! *****************************************************************************
+https://github.com/styfle/copee
+Copyright (c) styfle. All rights reserved. 
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0  
+***************************************************************************** */
+
+/**
+ * Copy text to the user's clipboard
+ */
+export function toClipboard(text: string): boolean {
+    
+    // Create element and select the text
+    const ta = document.createElement('textarea');
+    ta.value = text;
+    ta.cols = 1;
+    ta.rows = 1;
+    ta.style.color = 'transparent';
+    ta.style.border = 'none';
+    document.body.appendChild(ta);
+    ta.select();
+    
+    let success = false;
+    try {
+        success = document.execCommand('copy');
+    } catch (err) {
+        success = false;
+    }
+    
+    // Cleanup the element we created
+    document.body.removeChild(ta);
+    
+    return success;
+}
+
+/**
+ * Copy text from an input/textarea element to a user's clipboard
+ * @param el 
+ * @param preserveSelection 
+ */
+export function fromElement(el: HTMLInputElement | HTMLTextAreaElement, preserveSelection?: boolean) {
+    let start = 0;
+    let end = 0;
+
+    if (preserveSelection) {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    }
+
+    el.select();
+
+    var success = false;
+    try {
+        success = document.execCommand('copy');
+    } catch (err) {
+        success = false;
+    }
+
+    if (preserveSelection) {
+        (el as any).setSelectionRange(start, end);
+    }
+
+    return success;
+}
