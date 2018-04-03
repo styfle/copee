95c8be3b29c0383f1ec5bdce419c4c8c379696ba
Steven <steven@ceriously.com>
2018-04-02 20:16:54 -0400

    Rebuild with TS and Rollup

--- dist/copee.umd.js
+++ dist/copee.umd.js
@@ -1,62 +1,62 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.copee = {})));
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (factory((global.copee = {})));
 }(this, (function (exports) { 'use strict';
 
-/**
- * Copy text to the user's clipboard
- */
-function toClipboard(text) {
-    // Create element and select the text
-    const ta = document.createElement('textarea');
-    ta.value = text;
-    ta.cols = 1;
-    ta.rows = 1;
-    ta.style.color = 'transparent';
-    ta.style.border = 'none';
-    document.body.appendChild(ta);
-    ta.select();
-    let success = false;
-    try {
-        success = document.execCommand('copy');
+    /**
+     * Copy text to the user's clipboard
+     */
+    function toClipboard(text) {
+        // Create element and select the text
+        const ta = document.createElement('textarea');
+        ta.value = text;
+        ta.cols = 1;
+        ta.rows = 1;
+        ta.style.color = 'transparent';
+        ta.style.border = 'none';
+        document.body.appendChild(ta);
+        ta.select();
+        let success = false;
+        try {
+            success = document.execCommand('copy');
+        }
+        catch (err) {
+            success = false;
+        }
+        // Cleanup the element we created
+        document.body.removeChild(ta);
+        return success;
     }
-    catch (err) {
-        success = false;
+    /**
+     * Copy text from an input/textarea element to a user's clipboard
+     * @param el The <input> or <textarea> element
+     * @param preserveSelection True if text selection/highlight should be preserved after copying text
+     */
+    function fromElement(el, preserveSelection) {
+        let start = 0;
+        let end = 0;
+        let success = false;
+        if (preserveSelection) {
+            start = el.selectionStart || 0;
+            end = el.selectionEnd || 0;
+        }
+        el.select();
+        try {
+            success = document.execCommand('copy');
+        }
+        catch (err) {
+            success = false;
+        }
+        if (preserveSelection) {
+            el.setSelectionRange(start, end);
+        }
+        return success;
     }
-    // Cleanup the element we created
-    document.body.removeChild(ta);
-    return success;
-}
-/**
- * Copy text from an input/textarea element to a user's clipboard
- * @param el The <input> or <textarea> element
- * @param preserveSelection True if text selection/highlight should be preserved after copying text
- */
-function fromElement(el, preserveSelection) {
-    let start = 0;
-    let end = 0;
-    let success = false;
-    if (preserveSelection) {
-        start = el.selectionStart || 0;
-        end = el.selectionEnd || 0;
-    }
-    el.select();
-    try {
-        success = document.execCommand('copy');
-    }
-    catch (err) {
-        success = false;
-    }
-    if (preserveSelection) {
-        el.setSelectionRange(start, end);
-    }
-    return success;
-}
 
-exports.toClipboard = toClipboard;
-exports.fromElement = fromElement;
+    exports.toClipboard = toClipboard;
+    exports.fromElement = fromElement;
 
-Object.defineProperty(exports, '__esModule', { value: true });
+    Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
--- docs/iife.js
+++ docs/iife.js
@@ -1,23 +1,23 @@
 (function (copee) {
-'use strict';
+    'use strict';
 
-const q = (sel) => document.querySelector(sel);
+    const q = (sel) => document.querySelector(sel);
 
-const showAlert = (success) => {
-    const el = success ? q('.alert-success') : q('.alert-danger');
-    el.style.display = 'block';
-    setTimeout(() => el.style.display = 'none', 3000);
-};
+    const showAlert = (success) => {
+        const el = success ? q('.alert-success') : q('.alert-danger');
+        el.style.display = 'block';
+        setTimeout(() => el.style.display = 'none', 3000);
+    };
 
-q('#btn1').addEventListener('click', () => {
-    const success = copee.toClipboard('Magic happens here');
-    showAlert(success);
-});
+    q('#btn1').addEventListener('click', () => {
+        const success = copee.toClipboard('Magic happens here');
+        showAlert(success);
+    });
 
-q('#btn2').addEventListener('click', () => {
-    const el = q('#input2');
-    const success = copee.fromElement(el);
-    showAlert(success);
-});
+    q('#btn2').addEventListener('click', () => {
+        const el = q('#input2');
+        const success = copee.fromElement(el);
+        showAlert(success);
+    });
 
 }(copee));
