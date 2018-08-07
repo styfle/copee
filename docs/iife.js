92f4f5b73f34b3aef0b16c92670522eb9c7a133f
Steven <steven@ceriously.com>
2018-08-07 12:50:38 -0400

    Fix docs for IE 11
    IE 11 supports const but not arrow functions

--- docs/iife.js
+++ docs/iife.js
@@ -1,20 +1,20 @@
 (function (copee) {
     'use strict';
 
-    const q = (sel) => document.querySelector(sel);
+    function q(sel) { return document.querySelector(sel) }
 
-    const showAlert = (success) => {
+    function showAlert(success) {
         const el = success ? q('.alert-success') : q('.alert-danger');
         el.style.display = 'block';
         setTimeout(() => el.style.display = 'none', 3000);
     };
 
-    q('#btn1').addEventListener('click', () => {
+    q('#btn1').addEventListener('click', function () {
         const success = copee.toClipboard('Magic happens here');
         showAlert(success);
     });
 
-    q('#btn2').addEventListener('click', () => {
+    q('#btn2').addEventListener('click', function () {
         const el = q('#input2');
         const success = copee.fromElement(el);
         showAlert(success);
